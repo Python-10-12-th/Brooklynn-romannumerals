016d1926547b605ad12b8ff7beca1f79b1fafd39
BrooklynnDoming <domb270@aa.edu>
2025-03-06 08:34:38 -0700

    Test 2 testing Roman numeral II

--- testExample.py
+++ testExample.py
@@ -12,7 +12,12 @@ class TestRoman(unittest.TestCase):
     def test_I(self):
         from roman import convert_roman_numerals
         self.assertEqual(convert_roman_numerals("I"), 1)
+
+    def test_II(self):
+        from roman import convert_roman_numerals
+        self.assertEqual(convert_roman_numerals("II"), 2) 
         
 
 if __name__ == '__main__':
     unittest.main()
+
